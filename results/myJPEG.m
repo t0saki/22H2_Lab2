169639cca8059a3a7362c9bbb315de047a3313a3
tosaki-up <wzx1459931670@outlook.com>
2022-11-26 03:00:14 +0800

    move folder & edit high factor

--- results/myJPEG.m
+++ results/myJPEG.m
@@ -54,8 +54,9 @@ function myJPEG(fn)
 
     % quantization
     Qstd = [16 11 10 16 24 40 51 61; 12 12 14 19 26 58 60 55; 14 13 16 24 40 57 69 56; 14 17 22 29 51 87 80 62; 18 22 37 56 68 109 103 77; 24 35 55 64 81 104 113 92; 49 64 78 87 103 121 120 101; 72 92 95 98 112 100 103 99];
+    % set quantization factors
     factor_low = 0.42;
-    factor_high = 1/0.42;
+    factor_high = 2;
 
     % create quantization matrix
     Qlow = round(Qstd * factor_low);
